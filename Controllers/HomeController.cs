23635691b221451755d6bdb3892dd4bdd6b2aa05
mckay-moulton <79293626+mckay-moulton@users.noreply.github.com>
2022-03-25 22:35:02 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RecipesDbContext _context { get; set; }
+        //Constructor
+        public HomeController(RecipesDbContext temp)
+        {
+            _context = temp;
+        }
+        //Index view
+        public IActionResult Index()
+        {
+            var blah = _context.Recipes
+                .FromSqlRaw("Select * FROM Recipes ORDER BY RecipeTitle")
+                .ToList();
+            return View(blah);
+        }
+    }
+}
+
---
+++ Models/RecipesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Models
+{
+    public class RecipesDbContext : DbContext  
+    {
+        //inherit from base options
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options) : base (options)
+        {
+
+        }
+        //we are going to collect a set of individual recipes (singular), and when we bundle them together
+        //we will call it recipes (plural), that we can access.
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
